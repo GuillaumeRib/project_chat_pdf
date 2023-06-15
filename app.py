fef35cc2a7c37d0162e997a645a4158440b9561e
Guillaume Ribiere <gribiere@gmail.com>
2023-06-16 00:01:36 +0200

    streamlit app working local - API form - Browse files - Question prompt input - call model for answer

--- app.py
+++ app.py
@@ -1,64 +1,95 @@
 import streamlit as st
 import os
-
-openai_key = st.text_input('Input your OpenAI API key here')
-os.environ["OPENAI_API_KEY"] = openai_key
-
-
-file_path = TEMP
-
-# From raw PDF doc, load, split into smaller chunks before passing to Vectorizer & LLM
-from langchain.document_loaders import PyPDFLoader,UnstructuredPDFLoader
-loader = PyPDFLoader(file_path)
-documents = loader.load()
-print(len(documents))
-
-# Split the docs in chunks
-from langchain.text_splitter import RecursiveCharacterTextSplitter #text splitter
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=100)
-texts = text_splitter.split_documents(documents[0:135])
-
-# embedding function
 from langchain.embeddings import OpenAIEmbeddings
-open_ai_ef = OpenAIEmbeddings(model = 'text-embedding-ada-002') # default model - performing well and much cheaper
-
-# Chroma DB - Vectors DB
-from langchain.vectorstores import Chroma
-store = Chroma.from_documents(texts,
-                              embedding=open_ai_ef,
-                              collection_name='pdf_app',
-                             ) #store vectors into ChromaDB
-
-# Select llm model
 from langchain.chat_models import ChatOpenAI
-# Chat Model
-# "gpt-4, gpt-4-0613, gpt-4-32k, gpt-4-32k-0613, gpt-3.5-turbo, gpt-3.5-turbo-0613, gpt-3.5-turbo-16k, gpt-3.5-turbo-16k-0613"
-chat_open_ai_llm = ChatOpenAI(model='gpt-3.5-turbo',temperature=0.2) #init llm with temperature argument (control creativity)
+import openai
+from functions import *
+
+# Display the logo image in the second column
+col1, col2 = st.columns([1,1])
+#col1.image('logo-admin.png', width=300)
+col1.title('PDF Q&A by ChatGPT')
+col2.subheader('Ask questions to your PDF documents | Powered by Chat GPT 3.5 turbo')
+
+with st.expander("How it works ?"):
+    st.markdown('''
+                This tools aims to parse your own PDF documents, so that you can train LLMs on them and ask specific questions on their contents.\n
+                - Provide an API key from Open AI. It costs some :money: but not so much. If you dont have one, sign up on https://openai.com/ and get USD / EUR 5 free credits.
+                - Select a PDF document you want to talk to.
+                - Ask a question
+                - Get the model's answer ... and see the sources + pages that were used.
+                \b
+
+                Here below are the main tools / libraries that have been used:
+                * **PyPDFLoader, RecursiveCharacterTextSplitter** for PDF loading and parsing into smaller chunks
+                * **LangChain** for text preprocessing / pipeline
+                * **OpenAIEmbeddings** for embedding chunks into vectors
+                * **Chroma** vector database to store and search into vectors embedding (cost & time savings)
+                * **LLM: gpt-3.5-turbo** ChatModel from OpenAI
+                * **RetrievalQA** to call the model on chunks with highest similarity only instead of passing the full text (cost & time savings)
+
+                ''')
 
 
+st.subheader(f':closed_lock_with_key: API Key')
+openai_key = st.text_input("Enter your OpenAI API key",type='password')
 
-# Select number of chunks to pass to llm model after similarity performed on embeddings
-k = 8
-retriever = store.as_retriever(search_type='similarity',  # similarity or mmr
-                               search_kwargs={'k':k}
-                              )
+# TO DELETE AFTER
+# Set the API key as an environment variable
+
+if openai_key:
+    os.environ["OPENAI_API_KEY"] = openai_key
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+    st.markdown('API key loaded')
+
+    st.subheader(f':open_file_folder:PDF Uploader')
+    pdf_file = st.file_uploader('Select your PDF document')
+    if pdf_file:
+        with open('temp.pdf', 'wb') as temp_file:
+            temp_file.write(pdf_file.read())
+
+        fn = str(pdf_file.name).split('.')[0]
+
+        ### PARSING ###
+        documents, texts = parse_pdf('temp.pdf')
+        st.write(f"{len(texts)} text chunks created from {len(documents)} pages found in the pdf...   Storing embeddings into vectors' database...")
 
 
-# To select only the chunks with high similarity
-from langchain.chains import RetrievalQA
-qa = RetrievalQA.from_chain_type(
-    llm=chat_open_ai_llm,
-    chain_type='stuff', #refine / stuff (no refine for gpt 3.5 turbo)
-    retriever= retriever,
-    return_source_documents=True,
-)
+        from langchain.vectorstores import Chroma
+        from langchain.embeddings import OpenAIEmbeddings
+        # Embedding function
+        open_ai_ef = OpenAIEmbeddings(model = 'text-embedding-ada-002',openai_api_key=openai_key) # default model - performing well and much cheaper
 
-# Prompt
-prompt = st.text_input('Input your question here')
+        ### EMBEDDING ###
+        # Check if already in a persistent directory
+        # if not, create new vectordb
+        if os.path.exists(fn) is False:
+            persist_directory = fn
+            vectordb = Chroma.from_documents(texts,
+                            embedding=open_ai_ef,
+                            persist_directory=persist_directory)
+            vectordb.persist()
+            vectordb = None
 
-if prompt:
-    # Run Model for answer if prompt
-    answer = qa(prompt)
+        # if True, load it
+        else:
+            persist_directory = fn
+            vectordb = Chroma(persist_directory=persist_directory,embedding_function=open_ai_ef)
 
-    # Output on screen
-    st.write((answer['result'])
+
+        st.markdown('---')
+        ### PROMPT ###
+        st.subheader(f':thinking_face:Question')
+        prompt = st.text_input('Input your question here')
+
+        ### GET ANSWER FROM MODEL ###
+        if prompt:
+            st.markdown('---')
+            # Run Model for answer if prompt
+            with st.spinner("Looking into this for you ..."):
+                answer = get_answer(prompt,vectordb)
+                st.subheader(f':rocket:  Answer')
+                st.write(answer['result'])
+
+                with st.expander('Sources and Pages'):
+                    st.write([st.write(f'Page:{doc.metadata["page"]}\n---------------------------\n{doc.page_content}') for doc in answer['source_documents']][0])
---
+++ functions.py
@@ -0,0 +1,34 @@
+def parse_pdf(temp_file):
+    # From raw PDF doc, load, split into smaller chunks before passing to Vectorizer & LLM
+    from langchain.document_loaders import PyPDFLoader
+    loader = PyPDFLoader(temp_file)
+    documents = loader.load()
+
+    # Split the docs in chunks
+    from langchain.text_splitter import RecursiveCharacterTextSplitter #text splitter
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=100)
+    texts = text_splitter.split_documents(documents)
+    return documents, texts
+
+
+
+def get_answer(prompt,vectordb):
+    # Select llm model
+    from langchain.chat_models import ChatOpenAI
+    chat_open_ai_llm = ChatOpenAI(model='gpt-3.5-turbo',temperature=0.5)
+
+    # Select number of chunks to pass to llm model after similarity performed on embeddings
+    k = 4
+    retriever = vectordb.as_retriever(search_type='similarity',
+                                   search_kwargs={'k':k}
+                                            )
+
+    from langchain.chains import RetrievalQA
+    qa = RetrievalQA.from_chain_type(
+        llm=chat_open_ai_llm,
+        chain_type='stuff', #refine / stuff (no refine for gpt 3.5 turbo)
+        retriever= retriever,
+        return_source_documents=True,
+    )
+    answer = qa(prompt)
+    return answer
