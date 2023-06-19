6457fb73ed059c2d8aa178b8fc6d7c9c75e85a07
Guillaume Ribiere <gribiere@gmail.com>
2023-06-19 12:22:54 +0200

    edit page+1

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ with st.expander("How it works ?"):
                 - Select a PDF document you want to talk to.
                 - Ask a question
                 - Get the model's answer ... and see the sources + pages that were used.
-                \b
+
 
                 Here below are the main tools / libraries that have been used:
                 * **PyPDFLoader, RecursiveCharacterTextSplitter** for PDF loading and parsing into smaller chunks
@@ -34,6 +34,8 @@ with st.expander("How it works ?"):
 st.subheader(f':closed_lock_with_key: API Key')
 openai_key = st.text_input("Enter your OpenAI API key",type='password')
 # Set the API key as an environment variable
+
+### CHECK IF API KEY PROVIDED ###
 if openai_key:
     os.environ["OPENAI_API_KEY"] = openai_key
     openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -41,15 +43,19 @@ if openai_key:
 
     st.subheader(f':open_file_folder:  PDF Uploader')
     pdf_file = st.file_uploader('Select your PDF document')
+
+    ### CHECK IF PDF DOCUMENT UPLOADED ###
     if pdf_file:
+        # Reset the prompt
+
         with open('temp.pdf', 'wb') as temp_file:
             temp_file.write(pdf_file.read())
 
-        fn = str(pdf_file.name).split('.')[0]
+        fn = str(pdf_file.name).split('.')[0] # get file name to be used as folder to save doc vectors
 
         ### PARSING ###
         documents, texts = parse_pdf('temp.pdf')
-        st.write(f"{len(texts)} text chunks created from {len(documents)} pages found in the pdf...   Storing embeddings into vectors' database...")
+        st.write(f"{len(texts)} text chunks created from {len(documents)} pages found in the pdf...")
 
 
         from langchain.vectorstores import Chroma
@@ -58,20 +64,23 @@ if openai_key:
         open_ai_ef = OpenAIEmbeddings(model = 'text-embedding-ada-002',openai_api_key=openai_key) # default model - performing well and much cheaper
 
         ### EMBEDDING ###
-        # Check if already in a persistent directory
+        # Check if already in a persistent directory (file already loaded previously)
         # if not, create new vectordb
         if os.path.exists(fn) is False:
-            persist_directory = fn
-            vectordb = Chroma.from_documents(texts,
-                            embedding=open_ai_ef,
-                            persist_directory=persist_directory)
-            vectordb.persist()
-            vectordb = None
+            with st.spinner("Creating the vectors embedding ..."):
+                persist_directory = fn
+                vectordb = Chroma.from_documents(texts,
+                                embedding=open_ai_ef,
+                                persist_directory=persist_directory)
+                vectordb.persist()
+                #vectordb = None
+            st.write(f"Vectors embedding done. Stored in Vector DB.")
 
-        # if True, load it
+        # if True, load it instead of re-creating new embeddings
         else:
             persist_directory = fn
             vectordb = Chroma(persist_directory=persist_directory,embedding_function=open_ai_ef)
+            st.write(f"File already found. Existing vectors have been loaded.")
 
 
         st.markdown('---')
@@ -86,7 +95,8 @@ if openai_key:
             with st.spinner("Looking into this for you ..."):
                 answer = get_answer(prompt,vectordb)
                 st.subheader(f':rocket:  Answer')
+                st.markdown(f'**Question:** {prompt}')
                 st.write(answer['result'])
 
                 with st.expander('Sources and Pages'):
-                    st.write([st.write(f'Page:{doc.metadata["page"]}\n---------------------------\n{doc.page_content}') for doc in answer['source_documents']][0])
+                    st.write([st.write(f'Page:{doc.metadata["page"]+1}\n---------------------------\n{doc.page_content}') for doc in answer['source_documents']][0])
